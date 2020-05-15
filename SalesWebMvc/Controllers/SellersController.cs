8b4339e62080c205127099cdb441774b37da4d6a
Lucas Matheus <Lucasmatheusc@hotmail.com>
2020-05-15 09:38:39 -0300

    Update seller and custom service exception

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models.ViewModels;
 using SalesWebMvc.Models;
 using SalesWebMvc.Services;
-
+using SalesWebMvc.Services.Exceptions;
 
 namespace SalesWebMvc.Controllers
 {
@@ -42,13 +42,13 @@ namespace SalesWebMvc.Controllers
         }
         public IActionResult Delete(int? id)
         {
-            if(id == null)
+            if (id == null)
             {
                 return NotFound();
             }
             var obj = _sellerService.FindById(id.Value);
 
-            if(obj == null)
+            if (obj == null)
             {
                 return NotFound();
             }
@@ -75,6 +75,44 @@ namespace SalesWebMvc.Controllers
             }
             return View(obj);
 
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            List<Department> departments = _departmentServices.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { seller = obj, Departments = departments };
+            return View(viewModel);
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+            try
+            {
+                _sellerService.Update(seller);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundExceptions)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
         }
     }
 }
